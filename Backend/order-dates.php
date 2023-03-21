6fdf015196f7712960b840a3d4996a72b6c6b895
Shiyan Boxer <shiyanboxer7@gmail.com>
2023-03-21 13:49:49 -0400

    Finish order dates

---
+++ Backend/order-dates.php
@@ -0,0 +1,36 @@
+<?php
+include 'connectdb.php';
+
+echo "<html>";
+echo "<head>";
+echo "<title>Order Dates</title>";
+echo "<style type='text/css'>";
+include 'style.css';
+echo "</style>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Order Dates</h1>";
+echo "<table class='myTable'>";
+echo "<caption><h2>Order Counts by Date</h2></caption>";
+echo "<thead>";
+echo "<tr>";
+echo "<th width='50%'>Date</th>";
+echo "<th width='50%'>Number of Orders</th>";
+echo "</tr>";
+echo "</thead>";
+echo "<tbody>";
+
+$ordersQuery = $connection->prepare("SELECT DATE(OrderDate) AS OrderDate, COUNT(*) AS OrderCount FROM Restaurant_Order GROUP BY DATE(OrderDate)");
+$ordersQuery->execute();
+$orders = $ordersQuery->fetchAll();
+
+foreach ($orders as $order) {
+    echo "<tr><td>{$order['OrderDate']}</td><td>{$order['OrderCount']}</td></tr>";
+}
+
+echo "</tbody></table>";
+echo "</body>";
+echo "</html>";
+
+$connection = NULL;
+?>
